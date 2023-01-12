630f07d5a9589968f4452ebac26bb1263d4f6887
Badr <badrbiyadi@github.com>
2023-01-12 21:32:00 +0100

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { PostsComponent } from './posts/posts.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: 'new', component: PostFormComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    PostsComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
